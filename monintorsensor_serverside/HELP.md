2f8149331ae22d1a9cc232518023186c3ef5c1da
Yuriy Lukashevich <yuriy_lukashevich@epam.com>
2020-09-02 17:27:52 +0300

    serverside: dockerize project

---
+++ monintorsensor_serverside/HELP.md
@@ -0,0 +1,30 @@
+# Read Me First
+The following was discovered as part of building this project:
+
+* The original package name 'org.example.monitor-sensor' is invalid and this project uses 'org.example.monitorsensor' instead.
+
+# Getting Started
+
+### Reference Documentation
+For further reference, please consider the following sections:
+
+* [Official Apache Maven documentation](https://maven.apache.org/guides/index.html)
+* [Spring Boot Maven Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/2.3.3.RELEASE/maven-plugin/reference/html/)
+* [Create an OCI image](https://docs.spring.io/spring-boot/docs/2.3.3.RELEASE/maven-plugin/reference/html/#build-image)
+* [Spring Web](https://docs.spring.io/spring-boot/docs/2.3.3.RELEASE/reference/htmlsingle/#boot-features-developing-web-applications)
+* [Spring Data JPA](https://docs.spring.io/spring-boot/docs/2.3.3.RELEASE/reference/htmlsingle/#boot-features-jpa-and-spring-data)
+* [Spring Security](https://docs.spring.io/spring-boot/docs/2.3.3.RELEASE/reference/htmlsingle/#boot-features-security)
+* [OAuth2 Client](https://docs.spring.io/spring-boot/docs/2.3.3.RELEASE/reference/htmlsingle/#boot-features-security-oauth2-client)
+* [Liquibase Migration](https://docs.spring.io/spring-boot/docs/2.3.3.RELEASE/reference/htmlsingle/#howto-execute-liquibase-database-migrations-on-startup)
+
+### Guides
+The following guides illustrate how to use some features concretely:
+
+* [Building a RESTful Web Service](https://spring.io/guides/gs/rest-service/)
+* [Serving Web Content with Spring MVC](https://spring.io/guides/gs/serving-web-content/)
+* [Building REST services with Spring](https://spring.io/guides/tutorials/bookmarks/)
+* [Accessing Data with JPA](https://spring.io/guides/gs/accessing-data-jpa/)
+* [Securing a Web Application](https://spring.io/guides/gs/securing-web/)
+* [Spring Boot and OAuth2](https://spring.io/guides/tutorials/spring-boot-oauth2/)
+* [Authenticating a User with LDAP](https://spring.io/guides/gs/authenticating-ldap/)
+
--- monintorsensor_serverside/repository/src/main/resources/schema.sql
+++ monintorsensor_serverside/repository/src/main/resources/schema.sql
@@ -1,25 +1,28 @@
 -- Database: sensorMonitor
 -- DROP DATABASE "sensorMonitor";
-
-create DATABASE "sensorMonitor"
-    with
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
-------------------------------------------
+--
+-- create DATABASE "sensorMonitor"
+--     with
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     TABLESPACE = pg_default
+--     CONNECTION LIMIT = -1;
+-- ------------------------------------------
 
 -- Table: sensors
 -- DROP TABLE sensors;
 --------------------------------------------
 create TABLE IF NOT EXISTS sensors
 (
-    id               BIGSERIAL PRIMARY KEY NOT NULL,
+    id               BIGSERIAL             NOT NULL
+                constraint sensors_pkey PRIMARY KEY,
     name             VARCHAR(30)           NOT NULL,
     model            VARCHAR(15)           NOT NULL,
     range_from        BIGINT                       ,
     range_to          BIGINT                       ,
-    location         VARCHAR(40)                   ,
+    location_id      BIGINT
+                constraint fk_locations
+                REFERENCES locations                ,
     description      VARCHAR(200)
 );
 --------------------------------------------
@@ -34,14 +37,27 @@ create table if not exists locations
             UNIQUE
 );
 
+create table if not exists locations_sensors
+(
+    location_id bigint not null
+        constraint fk_locations_sensors_locations
+            REFERENCES locations,
+    sensors_id bigint not null
+        constraint uk_sensors
+            UNIQUE
+        constraint fk_location_sensors_sensors
+            REFERENCES sensors
+);
 
 -- Table: types
 -- DROP TABLE types;
 --------------------------------------------
 create TABLE IF NOT EXISTS types
 (
-    id      BIGSERIAL PRIMARY KEY NOT NULL,
-    name    VARCHAR(64)  UNIQUE   NOT NULL
+    id      BIGSERIAL PRIMARY KEY NOT NULL
+                    constraint types_pkey,
+    name    VARCHAR(64)            NOT NULL
+                constraint uk_types  UNIQUE
 );
 --------------------------------------------
 
@@ -63,13 +79,17 @@ create TABLE IF NOT EXISTS units
 ----------------------------------------------
 create TABLE IF NOT EXISTS users
 (
-    id         BIGSERIAL PRIMARY KEY NOT NULL,
+    id         BIGSERIAL  NOT NULL
+                constraint users_pkey
+                                  PRIMARY KEY,
     name       VARCHAR(20)           NOT NULL,
-    login      VARCHAR(30) UNIQUE    NOT NULL,
+    login      VARCHAR(30)           NOT NULL
+                    constraint uk_login
+                                        UNIQUE,
     password   VARCHAR(64),
     image_url   VARCHAR(1024),
-    last_visit TIMESTAMP,
-    provider   VARCHAR(30)
+    last_visit TIMESTAMP              NOT NULL,
+    provider   VARCHAR(255)           NOT NULL
 );
 -------------------------------------------------
 
@@ -78,8 +98,11 @@ create TABLE IF NOT EXISTS users
 -------------------------------------------------
 create TABLE IF NOT EXISTS roles
 (
-    id          BIGSERIAL PRIMARY KEY NOT NULL,
+    id          BIGSERIAL PRIMARY KEY NOT NULL
+            constraint roles_pkey,
     name        VARCHAR(30) UNIQUE    NOT NULL
+            constraint uk_roles
+                    unique
 );
 -------------------------------------------------
 
@@ -92,11 +115,14 @@ create UNIQUE INDEX IF NOT EXISTS roles_name_uindex
 ------------------------------------------
 create TABLE IF NOT EXISTS sensor_types
 (
-    sensor_id   bigint NOT NULL
-        REFERENCES sensors (id) MATCH SIMPLE
+    sensor_id   BIGINT NOT NULL
+     constraint sensor_types_pkey
+            PRIMARY KEY
+    constraint fk_sensors
+        REFERENCES sensors
             ON update CASCADE ON delete CASCADE,
-    type_id     bigint NOT NULL
-        REFERENCES types (id) MATCH SIMPLE
+    type_id     BIGINT NOT NULL
+         constraint fk_types REFERENCES types
             ON update CASCADE ON delete CASCADE
 );
 --------------------------------------------
@@ -106,11 +132,15 @@ create TABLE IF NOT EXISTS sensor_types
 --------------------------------------------
 create TABLE IF NOT EXISTS type_units
 (
-    unit_id bigint NOT NULL
-        REFERENCES units (id) MATCH SIMPLE
+    unit_id BIGINT NOT NULL
+        constraint fk_type_units_units
+                    REFERENCES units,
+            constraint type_units_pkey
+                 PRIMARY KEY (type_id, unit_id)
             ON update CASCADE ON delete CASCADE,
-    type_id  bigint NOT NULL
-        REFERENCES types (id) MATCH SIMPLE
+    type_id  BIGINT NOT NULL
+      constraint fk_type_units_types
+        REFERENCES types
             ON update CASCADE ON delete CASCADE
 );
 -------------------------------------------------
@@ -121,10 +151,12 @@ create TABLE IF NOT EXISTS type_units
 create TABLE IF NOT EXISTS users_roles
 (
     role_id BIGINT NOT NULL
-        REFERENCES roles (id) MATCH SIMPLE
+        constraint fk_roles
+        REFERENCES roles
             ON update CASCADE ON delete NO ACTION,
     user_id BIGINT NOT NULL
-        REFERENCES users (id) MATCH SIMPLE
+            constraint fk_users
+            REFERENCES users
             ON update CASCADE ON delete CASCADE
 );
 -------------------------------------------------
